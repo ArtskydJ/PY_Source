4321ecd8f3c929c0b0b7e35c5a9e746b302b96c8
Joseph Dykstra <josephdykstra@gmail.com>
2013-04-26 08:49:00 -0500

    source code: programming praxis; python challenge

---
+++ Programming Praxis/AnOddWayToSquare.py
@@ -0,0 +1,7 @@
+def square(n):
+    b=0
+    for i in range(n):
+        b+=n
+    return b
+
+print(square(201))
---
+++ ord.py
@@ -0,0 +1,4 @@
+for i in range (255):
+    print(i)
+    print(chr(i))
+    print("-----")
